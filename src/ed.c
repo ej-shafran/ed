aae89599bf07cc80c0f21519f69ad4cda70c02a6
ej-shafran <ejshafran@hilma.tech>
2023-09-20 23:28:02 +0300

    Keep only necessary declarations in `ed.h`

--- src/ed.c
+++ src/ed.c
@@ -5,6 +5,157 @@
 
 #include "ed.h"
 
+// LINE BUILDER
+
+// Dynamic array of lines (nul-terminated with the '\n' at the end)
+typedef da(char *) Ed_Line_Builder;
+
+// Read lines from `stream` into `lb` until `condition` is met.
+// Passing `""` as the condition will read until EOF.
+bool ed_lb_read_from_stream(Ed_Line_Builder *lb, FILE *file, char *condition);
+
+// Write all lines from `lb` into `stream`.
+#define ed_lb_write_to_stream(lb, file)          \
+	da_foreach(line, lb)                     \
+	{                                        \
+		int result = fputs(*line, file); \
+		if (result < 0)                  \
+			break;                   \
+	}
+
+// Insert the contents of `source` into `target` at index `address`,
+// pushing off the contents of `target` to make room.
+void ed_lb_insert_at(Ed_Line_Builder *target, Ed_Line_Builder *source,
+		     size_t address);
+
+// Insert the contents of `source` into `target` at index `address`,
+// while removing the line at `address` from the target.
+void ed_lb_pop_and_insert(Ed_Line_Builder *target, Ed_Line_Builder *source,
+			  size_t address);
+
+// Remove the line at `address` from `target`.
+void ed_lb_pop(Ed_Line_Builder *target, size_t address);
+
+// Wrapper around `ed_lb_read_from_stream` that reads lines from STDIN
+// until a line with just `"."` is encountered.
+#define ed_lb_read_to_dot(lb) ed_lb_read_from_stream(lb, stdin, ".\n")
+
+// Wrapper around `ed_lb_read_from_stream` that reads lines from `file` until EOF.
+#define ed_lb_read_file(lb, file) ed_lb_read_from_stream(lb, file, "")
+
+// Print lines from `lb` into `stream`
+#define ed_lb_fprint(stream, lb, start, end)                  \
+	do {                                                  \
+		size_t i = 0;                                    \
+		da_foreach(line, lb)                          \
+		{                                             \
+			i += 1;                               \
+			if (i >= start && i <= end)           \
+				fprintf(stream, "%s", *line); \
+		}                                             \
+	} while (0);
+
+// Wrapper around `ed_lb_fprint` which prints to STDOUT.
+#define ed_lb_print(lb, start, end) ed_lb_fprint(stdout, lb, start, end)
+
+// Print line numbers from `lb` into `stream`
+#define ed_lb_fnum(stream, lb, start, end)                  \
+	do {                                                \
+		size_t i = 0;                                  \
+		da_foreach(line, lb)                        \
+		{                                           \
+			i += 1;                             \
+			if (i >= start && i <= end)         \
+				fprintf(stream, "%zu\n", i); \
+		}                                           \
+	} while (0);
+
+// Wrapper around `ed_lb_fnum` which prints to STDOUT.
+#define ed_lb_num(lb, start, end) ed_lb_fnum(stdout, lb, start, end)
+
+// Print lines and their line numbers from `lb` into `stream`
+#define ed_lb_fprintn(stream, lb, start, end)                           \
+	do {                                                            \
+		size_t i = 0;                                              \
+		da_foreach(line, lb)                                    \
+		{                                                       \
+			i += 1;                                         \
+			if (i >= start && i <= end)                     \
+				fprintf(stream, "%zu     %s", i, *line); \
+		}                                                       \
+	} while (0);
+
+// Wrapper around `ed_lb_fprintn` which prints to STDOUT.
+#define ed_lb_printn(lb, start, end) ed_lb_fprintn(stdout, lb, start, end)
+
+// Free the allocated pointers in `lb`.
+#define ed_lb_free(lb)                  \
+	do {                            \
+		da_foreach(line, lb)    \
+		{                       \
+			free(*line);    \
+		}                       \
+		if (lb.items != NULL)   \
+			free(lb.items); \
+	} while (0);
+
+#define ed_lb_clear(lb)          \
+	do {                     \
+		ed_lb_free(lb);  \
+		lb.count = 0;    \
+		lb.items = NULL; \
+		lb.capacity = 0; \
+	} while (0);
+
+// COMMANDS
+
+typedef enum {
+	ED_CMD_APPEND = 0,
+	ED_CMD_INSERT,
+	ED_CMD_CHANGE,
+	ED_CMD_DELETE,
+	ED_CMD_PUT,
+	ED_CMD_EDIT,
+	ED_CMD_WRITE,
+	ED_CMD_NUM,
+	ED_CMD_PRINT,
+	ED_CMD_PRINT_NUM,
+	ED_CMD_QUIT,
+	ED_CMD_LAST_ERR,
+	ED_CMD_TOGGLE_PROMPT,
+	ED_CMD_TOGGLE_ERR,
+	ED_CMD_INVALID,
+	ED_CMD_JOIN,
+} Ed_Cmd_Type;
+
+typedef struct {
+	size_t start;
+	size_t end;
+} Ed_Range;
+
+typedef enum {
+	ED_ADDRESS_START = 0,
+	ED_ADDRESS_RANGE,
+	ED_ADDRESS_INVALID
+} Ed_Address_Type;
+
+typedef union {
+	size_t as_start;
+	Ed_Range as_range;
+} Ed_Address;
+
+// Parse a address from user input.
+//
+// Returns `ED_ADDRESS_INVALID` upon failure.
+// Sets `address` upon success, and `line` is updated to point to after the address.
+// The return value specifies which member of the union for `address` has been set.
+Ed_Address_Type ed_parse_address(char **line, Ed_Address *address);
+
+// Parse a command type from user input.
+//
+// Returns `ED_CMD_INVALID` upon failure.
+Ed_Cmd_Type ed_parse_cmd_type(char **line);
+
 typedef enum {
 	ED_ERROR_NO_ERROR = 0,
 	ED_ERROR_INVALID_COMMAND,
