ae2761ef9952f155a23da0eb566919f9eac0c369
ej-shafran <ejshafran@hilma.tech>
2023-10-02 00:24:15 +0300

    Update tests

--- test.bash
+++ test.bash
@@ -7,11 +7,9 @@ fail() {
     local expected="$1"
     local recieved="$2"
 
-    echo -e "[!] FAIL:" >&2
-    echo -en "\t" >&2
-    echo "Expected: '$expected'" >&2
-    echo -en "\t" >&2
-    echo "Recieved: '$recieved'" >&2
+    printf "\033[0;31m FAIL\033[0m\n" >&2
+    printf "\tExpected: '%s'\n" "$expected" >&2
+    printf "\tRecieved: '%s'\n" "$recieved" >&2
 
     TESTS_FAILED=$((TESTS_FAILED+1))
 }
@@ -24,33 +22,35 @@ runtest() {
     local commands="$1"
     local expected="$2"
 
-    echo -e "\nTest \`$TEST_NAME\`"
+    printf "\n%-50s" "$TEST_NAME:"
 
     local this
-    this="$(echo -e "$commands" | ./build/main 2>&1 | unescape)"
+    this="$(printf "%s" "$commands" | ./build/main 2>&1 | unescape)"
 
     if [ "$this" != "$expected" ]; then
         fail "$expected" "$this"
     else
-        echo "[*] SUCCESS"
+        printf "\033[0;32m SUCCESS\033[0m\n"
     fi
 }
 
-for dir in ./tests/*; do
-    COMMANDS="$dir/commands"
-    EXPECTED="$dir/expected"
-    TEST_NAME="$(basename "$dir")"
+for test_dir in ./tests/*; do
+    for dir in "$test_dir"/*; do
+        COMMANDS="$dir/commands"
+        EXPECTED="$dir/expected"
+        TEST_NAME="$dir"
 
-    runtest "$(cat "$COMMANDS")" "$(unescape < "$EXPECTED")"
+        runtest "$(cat "$COMMANDS")" "$(unescape < "$EXPECTED")"
+    done
 done
 
 if [ $TESTS_FAILED -eq 0 ]; then
-    echo -e "\nAll tests passed."
+    printf "\nAll tests passed.\n"
     true
 elif [ $TESTS_FAILED -eq 1 ]; then
-    echo -e "\n$TESTS_FAILED test failed." >&2
+    printf "\n%s test failed.\n" "$TESTS_FAILED" >&2
     false
 else
-    echo -e "\n$TESTS_FAILED tests failed." >&2
+    printf "\n%s tests failed.\n" "$TESTS_FAILED" >&2
     false
 fi
